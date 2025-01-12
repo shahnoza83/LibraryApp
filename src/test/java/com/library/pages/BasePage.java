1ad112fbf3adcdc94660d3d29587593cdaad86d7
shahnoza83 <168027836+shahnoza83@users.noreply.github.com>
2025-01-12 00:33:16 -0600

    Update BasePage.java

--- src/test/java/com/library/pages/BasePage.java
+++ src/test/java/com/library/pages/BasePage.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.PageFactory;
  * provides constructor with initElements method for re-usability
  * abstract - to prevent instantiation.
  */
+
 public class BasePage {
 
     public BasePage(){
